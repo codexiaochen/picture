78947f81c946ed1d870428a434b19691f8e1d085
abc <1163827926@qq.com>
2025-04-20 16:50:25 +0800

    配置常用依赖

--- src/main/java/com/chen/picture/PictureApplication.java
+++ src/main/java/com/chen/picture/PictureApplication.java
@@ -1,9 +1,13 @@
 package com.chen.picture;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
+@MapperScan("com.chen.picture.mapper")
+@EnableAspectJAutoProxy(exposeProxy = true) //可以在业务逻辑中访问到当前代理对象
 public class PictureApplication {
 
     public static void main(String[] args) {
