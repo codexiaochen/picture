18ed6879724dea907a688e8728d1026bbd9096c3
abc <1163827926@qq.com>
2025-04-20 20:51:18 +0800

    封装实体类

---
+++ src/main/java/com/chen/picture/mapper/UserMapper.java
@@ -0,0 +1,18 @@
+package com.chen.picture.mapper;
+
+import com.chen.picture.model.User;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+* @author abc
+* @description 针对表【user(用户)】的数据库操作Mapper
+* @createDate 2025-04-20 20:37:54
+* @Entity com.chen.picture.model.User
+*/
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
